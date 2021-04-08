f6be2c2158a33a3cc9bb4a0170d9be90a67ee98b
railanevaqueira <80965516+railanevaqueira@users.noreply.github.com>
2021-04-08 12:18:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const  botaoOcultar  =  documento . getElementById ( 'ocultar' )
-const  botaoMostrar  =  document . getElementById ( 'mostrar' )
-const  imagem  =  documento . getElementById ( 'imagem' )
-botaoMostrar . estilo . display  =  'nenhum'
+const botaoOcultar = document.getElementById('ocultar')
+const botaoMostrar = document.getElementById('mostrar')
+const imagem = document.getElementById('imagem')
+botaoMostrar.style.display = 'none'
 
-function  ocultarImagem ( )  {
-    imagem . estilo . display  =  'nenhum'
-    botaoOcultar . estilo . display  =  'nenhum'
-    botaoMostrar . estilo . display  =  'bloquear'
+function ocultarImagem() {
+    imagem.style.display = 'none'
+    botaoOcultar.style.display = 'none'
+    botaoMostrar.style.display = 'block'
 }
 
-function  mostrarImagem ( )  {
-    imagem . estilo . display  =  'bloquear'
-    botaoOcultar . estilo . display  =  'bloquear'
-    botaoMostrar . estilo . display  =  'nenhum'
+function mostrarImagem() {
+    imagem.style.display = 'block'
+    botaoOcultar.style.display = 'block'
+    botaoMostrar.style.display = 'none'
 }
